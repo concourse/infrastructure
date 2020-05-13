a991bce45ecba2e4a9f28d4d7cf75a6c2ab5469e
Aidan Oldershaw <aoldershaw@pivotal.io>
2020-05-13 16:44:52 -0400

    terraform/vault -> terraform/configure_vault
    This terraform module is only for configuring vault, not deploying it.
    The name was a little confusing, IMO.
    
    Signed-off-by: Aidan Oldershaw <aoldershaw@pivotal.io>

--- tasks/scripts/initialize-vault.sh
+++ tasks/scripts/initialize-vault.sh
@@ -87,7 +87,7 @@ pushd production-terraform/ > /dev/null
   vault_ca_cert="$(terraform output vault_ca_cert)"
 popd > /dev/null
 
-pushd greenpeace/terraform/vault > /dev/null
+pushd greenpeace/terraform/configure_vault > /dev/null
   export VAULT_TOKEN="${token}"
   export VAULT_ADDR="https://127.0.0.1:8200"
   export VAULT_SKIP_VERIFY="true"
