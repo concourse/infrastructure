c259e25f068c31efacbf3d2c4299e7711eb4481b
Aidan Oldershaw <aoldershaw@pivotal.io>
2020-10-14 14:26:20 -0400

    add metrics module to production cluster
    Signed-off-by: Aidan Oldershaw <aoldershaw@pivotal.io>
    Co-authored-by: Bishoy Youssef <byoussef@pivotal.io>

--- terraform/production/metrics.tf
+++ terraform/production/metrics.tf
@@ -1,6 +1,28 @@
+resource "kubernetes_namespace" "metrics" {
+  depends_on = [
+    module.cluster
+  ]
+
+  metadata {
+    name = "metrics"
+  }
+}
+
 module "cert_manager_issuer" {
   source = "../cert_manager_issuer"
 
   cluster_name = "production"
   project      = var.project
+}
+
+module "metrics" {
+  source = "../metrics"
+
+  namespace        = kubernetes_namespace.metrics.id
+  subdomain        = "metrics-ci-test"
+  node_pool        = "generic-1"
+  cert_name        = "production-metrics"
+  cert_secret_name = "production-metrics-tls"
+
+  namespace_regex = "ci|vault"
 }
