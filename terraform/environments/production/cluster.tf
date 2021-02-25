62dbbd6c4beff7071f07b72522e371ba99b1a784
Bohan Chen <bochen@pivotal.io>
2021-02-25 15:41:01 -0500

    add worker pool for ci-workers and ci-pr-workers
    Signed-off-by: Bohan Chen <bochen@pivotal.io>
    Co-authored-by: Aidan Oldershaw <aoldershaw@pivotal.io>

--- terraform/environments/production/cluster.tf
+++ terraform/environments/production/cluster.tf
@@ -11,14 +11,44 @@ module "cluster" {
   release_channel = "STABLE"
 
   node_pools = {
-    "generic-1" = {
+    "generic" = {
       auto_upgrade    = true
       disk_size       = "50"
       disk_type       = "pd-ssd"
       image           = "COS"
       local_ssds      = 0
       machine_type    = "n1-standard-8"
-      max             = 3
+      max             = 5
+      min             = 1
+      preemptible     = false
+      service_account = null
+      extra_oauth_scopes = [
+        "https://www.googleapis.com/auth/cloud-platform",
+      ]
+    },
+    "ci-workers" = {
+      auto_upgrade    = true
+      disk_size       = "50"
+      disk_type       = "pd-ssd"
+      image           = "COS"
+      local_ssds      = 0
+      machine_type    = "custom-8-16384"
+      max             = 10
+      min             = 1
+      preemptible     = false
+      service_account = null
+      extra_oauth_scopes = [
+        "https://www.googleapis.com/auth/cloud-platform",
+      ]
+    },
+    "ci-pr-workers" = {
+      auto_upgrade    = true
+      disk_size       = "50"
+      disk_type       = "pd-ssd"
+      image           = "COS"
+      local_ssds      = 0
+      machine_type    = "custom-8-16384"
+      max             = 5
       min             = 1
       preemptible     = false
       service_account = null
