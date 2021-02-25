cd897f4e676b93a577fc45c338ec7b61294dc005
Bohan Chen <bochen@pivotal.io>
2021-02-25 15:41:57 -0500

    add ci-pr helm release
    Signed-off-by: Bohan Chen <bochen@pivotal.io>
    Co-authored-by: Aidan Oldershaw <aoldershaw@pivotal.io>

---
+++ terraform/environments/production/ci-pr-workers-values.yml.tpl
@@ -0,0 +1,45 @@
+image: ${image_repo}
+imageDigest: ${image_digest}
+
+persistence:
+  worker:
+    storageClass: ssd
+    size: 750Gi
+
+worker:
+  replicas: 3
+  nodeSelector:
+    cloud.google.com/gke-nodepool: ci-pr-workers
+  annotations:
+    manual-update-revision: "1"
+  terminationGracePeriodSeconds: 300
+  updateStrategy:
+    rollingUpdate:
+      partition: 0
+  livenessProbe:
+    periodSeconds: 60
+    failureThreshold: 10
+    timeoutSeconds: 45
+  hardAntiAffinity: true
+  resources:
+    limits:   { cpu: 7500m, memory: 14Gi }
+    requests: { cpu: 0m,    memory: 0Gi  }
+
+concourse:
+  worker:
+    tag: "pr"
+    tsa:
+      hosts: ['${ci_release_name}-web-worker-gateway.${ci_namespace}.svc.cluster.local:2222']
+    rebalanceInterval: 2h
+    baggageclaim: { driver: overlay }
+    healthcheckTimeout: 40s
+    runtime: containerd
+    containerd:
+      networkPool: "10.254.0.0/16"
+      maxContainers: "500"
+      restrictedNetworks:
+        - "169.254.169.254/32"
+
+secrets:
+  hostKeyPub: ${host_key_pub}
+  workerKey: ${worker_key}
---
+++ terraform/environments/production/ci-pr.tf
@@ -0,0 +1,95 @@
+resource "kubernetes_namespace" "ci_pr" {
+  depends_on = [
+    module.cluster
+  ]
+
+  metadata {
+    name = "ci-pr"
+  }
+}
+
+data "template_file" "ci_pr_values" {
+  template = file("${path.module}/ci-pr-values.yml.tpl")
+  vars = {
+    image_repo   = var.concourse_image_repo
+    image_digest = var.concourse_image_digest
+
+    host_key_pub = jsonencode(tls_private_key.host_key.public_key_openssh)
+    worker_key   = jsonencode(tls_private_key.worker_key.private_key_pem)
+
+    ci_namespace    = kubernetes_namespace.ci.id
+    ci_release_name = helm_release.ci_concourse.metadata.name
+  }
+}
+
+resource "helm_release" "ci_pr_concourse" {
+  namespace  = kubernetes_namespace.ci_pr.id
+  name       = "ci-pr"
+  repository = "https://concourse-charts.storage.googleapis.com"
+  chart      = "concourse"
+  version    = "11.1.0"
+
+  timeout = 1800
+
+  values = [
+    data.template_file.ci_pr_values.rendered,
+  ]
+
+  depends_on = [
+    module.cluster.node_pools,
+  ]
+}
+
+resource "kubernetes_network_policy" "ci_pr_only_external" {
+  metadata {
+    name      = "only-external"
+    namespace = kubernetes_namespace.ci_pr.id
+  }
+
+  spec {
+    pod_selector {
+      match_labels {
+        release = helm_release.ci_pr.metadata.name
+      }
+    }
+
+    ingress {}
+
+    egress {
+      ports {
+        port     = "53"
+        protocol = "TCP"
+      }
+      ports {
+        port     = "53"
+        protocol = "UDP"
+      }
+
+      to {
+        namespace_selector {
+          match_labels = {
+            release = helm_release.ci_concourse.metadata.name
+          }
+        }
+        pod_selector {
+          match_labels {
+            app = "${helm_release.ci_concourse.metadata.name}-web"
+          }
+        }
+      }
+
+      to {
+        ip_block {
+          # allow any out
+          cidr = "0.0.0.0/0"
+          except = [
+            # except internal comms
+            "10.0.0.0/8",
+          ]
+        }
+      }
+    }
+
+    policy_types = ["Ingress", "Egress"]
+  }
+}
