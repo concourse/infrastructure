edc3e857085a7609caa4dcbceba5bd9408d80862
Aidan Oldershaw <aoldershaw@pivotal.io>
2021-03-02 11:55:03 -0500

    add k8s topgun cluster and worker
    Signed-off-by: Aidan Oldershaw <aoldershaw@pivotal.io>

---
+++ terraform/environments/production/k8s_topgun.tf
@@ -0,0 +1,121 @@
+module "k8s_topgun_cluster" {
+  source = "../../dependencies/cluster"
+
+  name    = "k8s_topgun"
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+
+  release_channel = "STABLE"
+
+  node_pools = {
+    "ci-workers" = {
+      auto_upgrade    = true
+      disk_size       = "20"
+      disk_type       = "pd-ssd"
+      image           = "COS"
+      local_ssds      = 0
+      machine_type    = "n1-standard-8"
+      max             = 3
+      min             = 1
+      preemptible     = false
+      service_account = null
+      extra_oauth_scopes = [
+        "https://www.googleapis.com/auth/cloud-platform",
+      ]
+    },
+    "cos" = {
+      auto_upgrade    = true
+      disk_size       = "100"
+      disk_type       = "pd-ssd"
+      image           = "Ubuntu"
+      local_ssds      = 0
+      machine_type    = "n1-highcpu-8"
+      max             = 10
+      min             = 1
+      preemptible     = false
+      service_account = null
+      extra_oauth_scopes = [
+        "https://www.googleapis.com/auth/cloud-platform",
+      ]
+      labels = {
+        nodeImage = "cos"
+      }
+    },
+    "ubuntu" = {
+      auto_upgrade    = true
+      disk_size       = "100"
+      disk_type       = "pd-ssd"
+      image           = "COS"
+      local_ssds      = 0
+      machine_type    = "n1-highcpu-8"
+      max             = 10
+      min             = 1
+      preemptible     = false
+      service_account = null
+      extra_oauth_scopes = [
+        "https://www.googleapis.com/auth/cloud-platform",
+      ]
+      labels = {
+        nodeImage = "ubuntu"
+      }
+    },
+  }
+}
+
+resource "kubernetes_storage_class" "ssd" {
+  provider = kubernetes.k8s_topgun
+
+  metadata {
+    name = "ssd"
+  }
+  storage_provisioner = "kubernetes.io/gce-pd"
+  reclaim_policy      = "Delete"
+  parameters = {
+    type = "pd-ssd"
+  }
+  volume_binding_mode = "Immediate"
+}
+
+resource "kubernetes_namespace" "ci_topgun_worker" {
+  provider = kubernetes.k8s_topgun
+
+  depends_on = [
+    module.cluster
+  ]
+
+  metadata {
+    name = "ci-topgun-worker"
+  }
+}
+
+data "template_file" "ci_topgun_worker_values" {
+  template = file("${path.module}/ci-topgun-worker-values.yml.tpl")
+  vars = {
+    image_repo   = var.concourse_image_repo
+    image_digest = var.concourse_image_digest
+
+    host_key_pub = jsonencode(tls_private_key.host_key.public_key_openssh)
+    worker_key   = jsonencode(tls_private_key.worker_key.private_key_pem)
+
+    host = "https://${var.subdomain}.${var.domain}:2222"
+  }
+}
+
+resource "helm_release" "k8s_topgun_worker" {
+  namespace  = kubernetes_namespace.ci_topgun_worker.id
+  name       = "topgun-worker"
+  repository = "https://concourse-charts.storage.googleapis.com"
+  chart      = "concourse"
+  version    = "11.1.0"
+
+  timeout = 1800
+
+  values = [
+    data.template_file.ci_topgun_worker_values.rendered,
+  ]
+
+  depends_on = [
+    module.k8s_topgun_cluster.node_pools,
+  ]
+}
---
+++ terraform/environments/production/k8s_topgun_worker-values.yml.tpl
@@ -0,0 +1,51 @@
+image: ${image_repo}
+imageDigest: ${image_digest}
+
+postgresql:
+  enabled: false
+
+web:
+  enabled: false
+
+persistence:
+  worker:
+    storageClass: ssd
+    size: 100Gi
+
+worker:
+  replicas: 1
+  nodeSelector:
+    cloud.google.com/gke-nodepool: topgun-workers
+  annotations:
+    manual-update-revision: "1"
+  terminationGracePeriodSeconds: 300
+  updateStrategy:
+    rollingUpdate:
+      partition: 0
+  livenessProbe:
+    periodSeconds: 60
+    failureThreshold: 10
+    timeoutSeconds: 45
+  hardAntiAffinity: true
+  resources:
+    limits:   { cpu: 7500m, memory: 14Gi }
+    requests: { cpu: 0m,    memory: 0Gi  }
+
+concourse:
+  worker:
+    tag: "k8s-topgun"
+    tsa:
+      hosts: ["${host}"]
+    rebalanceInterval: 2h
+    baggageclaim: { driver: overlay }
+    healthcheckTimeout: 40s
+    runtime: containerd
+    containerd:
+      networkPool: "10.254.0.0/16"
+      maxContainers: "500"
+      restrictedNetworks:
+        - "169.254.169.254/32"
+
+secrets:
+  hostKeyPub: ${host_key_pub}
+  workerKey: ${worker_key}
