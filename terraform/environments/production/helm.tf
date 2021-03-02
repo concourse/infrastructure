685b9574128baa2c0cb56b99ab157df0f9e06358
Aidan Oldershaw <aoldershaw@pivotal.io>
2021-03-02 11:55:03 -0500

    add k8s topgun cluster and worker
    Signed-off-by: Aidan Oldershaw <aoldershaw@pivotal.io>

--- terraform/environments/production/helm.tf
+++ terraform/environments/production/helm.tf
@@ -8,3 +8,16 @@ provider "helm" {
     cluster_ca_certificate = base64decode(module.cluster.cluster_ca_certificate)
   }
 }
+
+provider "helm" {
+  alias = "k8s_topgun"
+
+  kubernetes {
+    host = "https://${module.k8s_topgun_cluster.endpoint}"
+
+    username = module.k8s_topgun_cluster.username
+    password = module.k8s_topgun_cluster.password
+
+    cluster_ca_certificate = base64decode(module.k8s_topgun_cluster.cluster_ca_certificate)
+  }
+}
