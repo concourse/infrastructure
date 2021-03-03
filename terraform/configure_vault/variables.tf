7920bcbdc1ecc40e8496687eb87b89dfa26c1ac7
Aidan Oldershaw <aoldershaw@pivotal.io>
2021-03-03 12:14:14 -0500

    remove configure_vault terraform module
    Signed-off-by: Aidan Oldershaw <aoldershaw@pivotal.io>

--- terraform/configure_vault/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "concourse_cert" {
-  type        = string
-  description = "CA cert that vault will use to validate Concourse's cert for authentication"
-}
--- terraform/configure_vault/versions.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  required_providers {
-    vault = {
-      source = "hashicorp/vault"
-    }
-  }
-  required_version = ">= 0.13"
-}
