4a2ebefd1b68c898866b8f641f53ede3aa413ae4
Esteban Foronda <eforonda@vmware.com>
2021-03-03 16:15:26 -0500

    allow overriding compute address name
    to avoid conflicts with existing resources
    
    Signed-off-by: Esteban Foronda <eforonda@vmware.com>
    Co-authored-by: Aidan Oldershaw <aoldershaw@pivotal.io>

--- terraform/dependencies/address/variables.tf
+++ terraform/dependencies/address/variables.tf
@@ -7,3 +7,8 @@ variable "subdomain" {
   description = "Subdomain under the DNS zone to register"
   type        = string
 }
+
+variable "compute_address_name" {
+  description = "name for compute address. defaults to subdomain"
+  default     = null
+}
