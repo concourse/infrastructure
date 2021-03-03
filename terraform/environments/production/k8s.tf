e692d0f314aa3492860c351d3c235f2f88579c35
Aidan Oldershaw <aoldershaw@pivotal.io>
2021-03-03 08:42:41 -0500

    re-enable providers
    Signed-off-by: Aidan Oldershaw <aoldershaw@pivotal.io>

--- terraform/environments/production/k8s.tf
+++ terraform/environments/production/k8s.tf
@@ -7,13 +7,13 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(module.cluster.cluster_ca_certificate)
 }
 
-#provider "kubernetes" {
-#  alias = "k8s_topgun"
-#
-#  host = "https://${module.k8s_topgun_cluster.endpoint}"
-#
-#  username = module.k8s_topgun_cluster.username
-#  password = module.k8s_topgun_cluster.password
-#
-#  cluster_ca_certificate = base64decode(module.k8s_topgun_cluster.cluster_ca_certificate)
-#}
+provider "kubernetes" {
+  alias = "k8s_topgun"
+
+  host = "https://${module.k8s_topgun_cluster.endpoint}"
+
+  username = module.k8s_topgun_cluster.username
+  password = module.k8s_topgun_cluster.password
+
+  cluster_ca_certificate = base64decode(module.k8s_topgun_cluster.cluster_ca_certificate)
+}
