92351ae87a73bf5121aa205025ae8735f7525199
Aidan Oldershaw <aoldershaw@pivotal.io>
2021-03-03 00:08:10 -0500

    re-enable vault
    Signed-off-by: Aidan Oldershaw <aoldershaw@pivotal.io>

--- terraform/environments/production/vault.tf
+++ terraform/environments/production/vault.tf
@@ -1,13 +1,13 @@
-#module "vault" {
-#  source = "../../dependencies/vault"
-#
-#  gcp_service_account_id           = "production-vault"
-#  gcp_service_account_display_name = "Production Vault"
-#  gcp_service_account_description  = "Used to operate Vault in our Production cluster."
-#
-#  bucket_name = "concourse-production-vault"
-#
-#  depends_on = [
-#    module.cluster.node_pools
-#  ]
-#}
+module "vault" {
+  source = "../../dependencies/vault"
+
+  gcp_service_account_id           = "production-vault"
+  gcp_service_account_display_name = "Production Vault"
+  gcp_service_account_description  = "Used to operate Vault in our Production cluster."
+
+  bucket_name = "concourse-production-vault"
+
+  depends_on = [
+    module.cluster.node_pools
+  ]
+}
