7388f8de558caded6c3564230e670b66e601aaf3
Aidan Oldershaw <aoldershaw@pivotal.io>
2021-08-25 15:39:29 -0400

    fix stress address
    Signed-off-by: Aidan Oldershaw <aoldershaw@pivotal.io>

--- terraform/environments/stress/dns.tf
+++ terraform/environments/stress/dns.tf
@@ -5,7 +5,7 @@ module "concourse_stress_address" {
   source = "../../dependencies/address"
 
   dns_zone  = var.dns_zone
-  subdomain = var.baseline_subdomain
+  subdomain = var.stress_subdomain
 }
 
 # Reserves an address for `baseline.concourse-ci.org` and ties it
