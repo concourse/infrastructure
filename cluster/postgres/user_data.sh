ae2eda78892c1698eb05f6a40e66c793d11c5b9c
Taylor Silva <dev@taydev.net>
2024-09-14 22:18:04 -0400

    remove vault init and use pg tf state backend
    Signed-off-by: Taylor Silva <dev@taydev.net>

--- cluster/postgres/user_data.sh
+++ cluster/postgres/user_data.sh
@@ -18,18 +18,6 @@ volume=$(ls /mnt)
 volume_dir="/mnt/$volume"
 mkdir -p "$volume_dir/data"
 
-cat << EOF > vault-init.sql
-CREATE TABLE IF NOT EXISTS vault_kv_store (
-  parent_path TEXT COLLATE "C" NOT NULL,
-  path        TEXT COLLATE "C",
-  key         TEXT COLLATE "C",
-  value       BYTEA,
-  CONSTRAINT pkey PRIMARY KEY (path, key)
-);
-
-CREATE INDEX IF NOT EXISTS parent_path_idx ON vault_kv_store (parent_path);
-EOF
-
 cat << EOF > compose.yml
 services:
   postgres:
--- cluster/providers.tf
+++ cluster/providers.tf
@@ -1,5 +1,9 @@
 terraform {
-  backend "local" {}
+  backend "pg" {
+    # Set env vars PGUSER and PGPASSWORD to access the state file
+    conn_str    = "postgres://postgres/terraform_backend?sslmode=disable"
+    schema_name = "concourse-web"
+  }
 }
 
 variable "hcloud_token" {
