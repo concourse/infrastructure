ad4cdcb5bb1b41d9c1eb820bf2bef4f074f8a2ec
Taylor Silva <dev@taydev.net>
2024-09-14 22:42:16 -0400

    init web and worker modules
    Signed-off-by: Taylor Silva <dev@taydev.net>

--- cluster/web/providers.tf
+++ cluster/web/providers.tf
@@ -2,7 +2,14 @@ terraform {
   backend "pg" {
     # Set env vars PGUSER and PGPASSWORD to access the state file
     conn_str    = "postgres://postgres/terraform_backend?sslmode=disable"
-    schema_name = "concourse-web"
+    schema_name = "concourse_web"
+  }
+
+  required_providers {
+    hcloud = {
+      source  = "hetznercloud/hcloud"
+      version = "~> 1.45"
+    }
   }
 }
 
---
+++ cluster/workers/providers.tf
@@ -0,0 +1,23 @@
+terraform {
+  backend "pg" {
+    # Set env vars PGUSER and PGPASSWORD to access the state file
+    conn_str    = "postgres://postgres/terraform_backend?sslmode=disable"
+    schema_name = "concourse_web"
+  }
+
+  required_providers {
+    hcloud = {
+      source  = "hetznercloud/hcloud"
+      version = "~> 1.45"
+    }
+  }
+}
+
+variable "hcloud_token" {
+  type      = string
+  sensitive = true
+}
+
+provider "hcloud" {
+  token = var.hcloud_token
+}
