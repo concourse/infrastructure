e1b870ce5c90012bf84ea5a24598004a929f6388
Taylor Silva <dev@taydev.net>
2024-09-15 14:13:23 -0400

    enable docker service
    Signed-off-by: Taylor Silva <dev@taydev.net>

--- cluster/postgres/user_data.sh
+++ cluster/postgres/user_data.sh
@@ -14,6 +14,7 @@ sudo tailscale up --ssh
 
 sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
 sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl enable docker
 sudo systemctl start docker
 
 volume=$(ls /mnt)
