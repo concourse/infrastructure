8fd086cf123b40b675999104b966cfd21e496d97
Taylor Silva <dev@taydev.net>
2024-09-15 14:12:53 -0400

    need to enable ip_forward
    Signed-off-by: Taylor Silva <dev@taydev.net>

--- network/user_data.sh
+++ network/user_data.sh
@@ -11,10 +11,12 @@ sudo tailscale up --ssh
 
 sudo dnf -y install iptables
 
+echo 1 > /proc/sys/net/ipv4/ip_forward
 iptables -t nat -A POSTROUTING -s '10.6.0.0/16' -o eth0 -j MASQUERADE
 
 # Persist NAT setup after reboots
 cat <<EOF > /etc/NetworkManager/dispatcher.d/ifup-local
+echo 1 > /proc/sys/net/ipv4/ip_forward
 /usr/sbin/iptables -t nat -A POSTROUTING -s '10.6.0.0/16' -o eth0 -j MASQUERADE
 EOF
 chmod +x /etc/NetworkManager/dispatcher.d/ifup-local
