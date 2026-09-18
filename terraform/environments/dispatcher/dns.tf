23e31d0e3e80b7cb8ba884ba96f2822cf642893e
Taylor Silva <dev@taydev.net>
2026-09-18 12:44:46 -0400

    remove dispatcher
    I also removed all resources from GCP that were leftover.
    
    Signed-off-by: Taylor Silva <dev@taydev.net>

--- terraform/environments/dispatcher/dns.tf
+++
@@ -1,9 +0,0 @@
-# Reserves an address for `dispatcher.concourse-ci.org` and ties it
-# to the given domain.
-#
-module "concourse_dispatcher_address" {
-  source = "../../dependencies/address"
-
-  dns_zone  = var.dns_zone
-  subdomain = var.subdomain
-}
--- terraform/environments/dispatcher/k8s.tf
+++
@@ -1,9 +0,0 @@
-data "google_client_config" "provider" {}
-
-provider "kubernetes" {
-  host = "https://${module.cluster.endpoint}"
-
-  token = data.google_client_config.provider.access_token
-
-  cluster_ca_certificate = base64decode(module.cluster.cluster_ca_certificate)
-}
